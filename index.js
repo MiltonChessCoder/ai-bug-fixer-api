581d531ae21f5136687ca5da00591d7d516b64b8
Milton <miltonsoftwareengineer@gmail.com>
2026-02-23 16:58:04 +0300

    automated status route

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
   res.send('Bug fixers are here to help!');
 });
 
+app.get('/status', (req, res) => {
+  res.json({ status: "ok" });
+});
+
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
